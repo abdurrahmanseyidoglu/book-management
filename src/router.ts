9c0a4c7804bf13322ca2df2692d47c73da87498f
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2024-09-02 16:43:53 +0300

    refactor routes (taken from default Vue application)

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,16 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
-
 import HomeView from './views/HomeView.vue'
 import BookView from './views/BookView.vue'
+import AddBook from './views/AddBook.vue'
 
-const routes = [
-  { path: '/', component: HomeView },
-  { path: '/:id', component: BookView }
-]
+import { createRouter, createWebHistory } from 'vue-router'
 
-export const router = createRouter({
-  history: createMemoryHistory(),
-  routes
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/:id', name: 'singleBook', component: BookView },
+    { path: '/add-book', name: 'addBook', component: AddBook }
+  ]
 })
+
+export default router
